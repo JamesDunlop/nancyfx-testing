a4ce5bfb6ac85c9781f5c53fec405b9d8ea36714
James Dunlop <jamesd@campaignmonitor.com>
2015-09-22 18:02:21 +1000

    Initial commit - trying out asp.net hosting

---
+++ NancyFx/NancyFxAspNet/HomeModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace NancyFxAspNet
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => "Hello from Nancy";
+            Get["/home"] = _ => View["Home"];
+        }
+    }
+}
